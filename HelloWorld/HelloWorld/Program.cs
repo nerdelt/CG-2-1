4f87de2d8ce97491038606ce5f8c56c62253cb66
nerdelt <nerdelt@gmail.com>
2018-07-18 10:33:03 -0500

    CG-2-1 Assignment

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            Console.Write("Please type your first name: ");
+            string myFirstName = Console.ReadLine();
+
+            Console.Write("Please type your last name: ");
+            string myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName + "!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
